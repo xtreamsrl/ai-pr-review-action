4bfb124e946240ef20171644bec71914c04cfb00
zoncrd <riccardo.zoncada@xtreamers.io>
2023-08-03 15:37:44 +0200

    feat: improve error handling on octokit failure

--- src/octokit.ts
+++ src/octokit.ts
@@ -5,7 +5,13 @@ import { error, warning } from '@actions/core';
 
 export const octokit = new Octokit();
 
-const generateCommentData = (comment: ReviewComment) => {
+type CommentData = {
+  path: string;
+  body: string;
+  line: number;
+  side: 'RIGHT';
+}
+const generateCommentData = (comment: ReviewComment): CommentData => {
   return {
     path: comment.fileName,
     body: comment.message,
@@ -14,24 +20,55 @@ const generateCommentData = (comment: ReviewComment) => {
   }
 }
 
+async function createReviewWithComments(commentsData: CommentData[], prNumber: number, commitId: string, context: ContextWithPullRequest) {
+  return await octokit.pulls.createReview({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: prNumber,
+    commit_id: commitId,
+    comments: commentsData
+  });
+}
+
+async function createReviewThenComments(commentsData: CommentData[], prNumber: number, commitId: string, context: ContextWithPullRequest) {
+  const review = await octokit.pulls.createReview({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: prNumber,
+    commit_id: commitId
+  });
+  for (const commentData of commentsData) {
+    try {
+      await octokit.pulls.createReviewComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: prNumber,
+        commit_id: commitId,
+        ...commentData
+      });
+    } catch (e: unknown) {
+      warning(`Failed to create comment on pr #${prNumber} with data: ${JSON.stringify(commentData)}`);
+    }
+  }
+  return review;
+}
+
 export async function submitReview(comments: ReviewComment[], prNumber: number, commitId: string, context: ContextWithPullRequest) {
   const commentsData = comments.map(comment =>
     generateCommentData(comment)
   );
   try {
-    const review = await octokit.pulls.createReview({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      pull_number: prNumber,
-      commit_id: commitId,
-      comments: commentsData
-    });
+    let review;
+    try {
+      review = await createReviewWithComments(commentsData, prNumber, commitId, context);
+    } catch (e: unknown) {
+      // Try to create review and submit comment separately, to avoid missing the entire review if one comment fails.
+      review = await createReviewThenComments(commentsData, prNumber, commitId, context);
+    }
     await octokit.pulls.submitReview({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      // eslint-disable-next-line camelcase
       pull_number: prNumber,
-      // eslint-disable-next-line camelcase
       review_id: review.data.id,
       event: 'COMMENT'
     })
