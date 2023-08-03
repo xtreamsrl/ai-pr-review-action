d211247934d7bf80c349a9c1acf5acaa6585bb1c
zoncrd <riccardo.zoncada@xtreamers.io>
2023-08-03 14:49:47 +0200

    feat: improve error handling on octokit failure

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,6 +1,7 @@
 import { Octokit } from '@octokit/action';
 import { ContextWithPullRequest } from './utils';
 import { ReviewComment } from './reviewer';
+import { error, warning } from '@actions/core';
 
 export const octokit = new Octokit();
 
@@ -13,22 +14,30 @@ const generateCommentData = (comment: ReviewComment) => {
 }
 
 export async function submitReview(comments: ReviewComment[], prNumber: number, commitId: string, context: ContextWithPullRequest) {
-  const review = await octokit.pulls.createReview({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    pull_number: prNumber,
-    commit_id: commitId,
-    comments: comments.map(comment =>
-      generateCommentData(comment)
-    )
-  });
-  await octokit.pulls.submitReview({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    // eslint-disable-next-line camelcase
-    pull_number: prNumber,
-    // eslint-disable-next-line camelcase
-    review_id: review.data.id,
-    event: 'COMMENT'
-  })
+  const commentsData = comments.map(comment =>
+    generateCommentData(comment)
+  );
+  try {
+    const review = await octokit.pulls.createReview({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: prNumber,
+      commit_id: commitId,
+      comments: commentsData
+    });
+    await octokit.pulls.submitReview({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      // eslint-disable-next-line camelcase
+      pull_number: prNumber,
+      // eslint-disable-next-line camelcase
+      review_id: review.data.id,
+      event: 'COMMENT'
+    })
+  } catch (e: unknown) {
+    error(`Failed to submit review on pr #${prNumber}`);
+    warning(`Comments data: ${JSON.stringify(commentsData)}`);
+    throw e;
+  }
+
 }
